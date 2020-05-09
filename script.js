ac098fa7cf9da9019dc17d1ea4c3c97d28bb63e2
Dmitriy_Kolesnikov <nonameFOX@yandex.ru>
2020-05-09 17:57:30 +0300

    add promise

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
-var search = new URLSearchParams(window.location.search);
-	if (search.has('username')) {
-		var userName = search.get('username');
-		fetch('https://api.github.com/users/'+userName)
-			.then(res => res.json())
-			.then(json => {
-				if (json.bio === null) {json.bio = 'Информация о себе отсутствует'}
-				if (json.name === null) {json.name = 'Имя пользователя отсутствует'}
-				if (json.message === "Not Found"){
-					document.body.innerHTML = "ошибка: информация о пользователе не доступна."
-				}
-				else {
-					document.body.innerHTML = '<h2>' + '<a href="' + json.html_url + '">' +  
-						json.name + '</a>' + '</h2>' + '<br>' +
-						'<img src="'+json.avatar_url + '">' + '<br>' +
-						'<p>' + json.bio + '</p>'
-				}
-			})
-	}
-	else {
-		document.body.innerHTML = "ошибка: не добавлен адрес пользователя";
+var	elBody = document.getElementById('body'),
+	elLoad = document.getElementById('preload'),
+	date = new Date,
+	search = new URLSearchParams(window.location.search),
+	userName = search.get('username'),
+	gtDate = new Promise((resolve, reject) => {
+	setTimeout(() => date ? resolve(date) : reject('error'), 1500)}),
+	getName = new Promise((resolve, reject) => {
+	setTimeout(() => userName ? resolve(userName) : reject('имя не найдено'), 3000)}),
+	preload = setTimeout(() => {
+      elBody.style.display = 'block';
+      elLoad.style.display = 'none';
+	}, 2000),
+	preloadOpacity = setTimeout(() => {
+        elBody.style.opacity = 1;
+    }, 2100);
+
+if (search.has('username')) {
+	Promise.all([gtDate, getName])
+	fetch('https://api.github.com/users/'+userName)
+		.then(res => res.json())
+		.then(json => {
+			if (json.bio === null) {json.bio = 'Информация о себе отсутствует'}
+			if (json.name === null) {json.name = 'Имя пользователя отсутствует'}
+			if (json.message === "Not Found"){
+				elBody.innerHTML = "ошибка: информация о пользователе не доступна."
+			}
+			else {
+				elBody.innerHTML = '<h2>' + '<a href="' + json.html_url + '">' +  
+					json.name + '</a>' + '</h2>' + '<br>' +
+					'<img src="'+json.avatar_url + '">' + '<br>' +
+					'<p>' + json.bio + '</p>' +
+					'<p>' + date + '</p>'
+			}
+		})
+}
+else {
+	document.body.innerHTML = "ошибка: не добавлен адрес пользователя";
 };
