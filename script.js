b30faf2b5a9197d1541ce07d44c63c4d0520ab78
Dmitriy_Kolesnikov <nonameFOX@yandex.ru>
2020-05-11 20:26:22 +0300

    добавил отображене моей страницы, если user не указан

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 const elBody = document.getElementById('body'),
-	elLoad = document.getElementById('preload'),
-	date = new Date,
-	search = new URLSearchParams(window.location.search),
-	userName = search.get('username'),
-	gtDate = new Promise((resolve, reject) => {
-	setTimeout(() => date ? resolve(date) : reject('error'), 1500)}),
-	getName = new Promise((resolve, reject) => {
-	setTimeout(() => userName ? resolve(userName) : reject('имя не найдено'), 3000)}),
-	preload = setTimeout(() => {
+	  elLoad = document.getElementById('preload'),
+      date = new Date,
+	  search = new URLSearchParams(window.location.search),
+	  userName = search.get('username'),
+	  userNameMain = 'tytytyw',
+	  gtDate = new Promise((resolve, reject) => {
+	  setTimeout(() => date ? resolve(date) : reject('error'), 1500)}),
+	  getName = new Promise((resolve, reject) => {
+	  setTimeout(() => userName ? resolve(userName) : reject('имя не найдено, отображается пользователь по умолчанию'), 3000)}),
+	  preload = setTimeout(() => {
       elBody.style.display = 'block';
       elLoad.style.display = 'none';
-	}, 2000),
+		}, 2000),
 	preloadOpacity = setTimeout(() => {
         elBody.style.opacity = 1;
-    }, 2100);
-
-if (search.has('username')) {
-	let requestfetch = fetch('https://api.github.com/users/'+userName)
+    	}, 2100);
+function UserInfo(Name){let requestfetch = fetch('https://api.github.com/users/'+Name)
 		.then(res => res.json())
 		.then(json => {
 			if (json.bio === null) {json.bio = 'Информация о себе отсутствует'}
@@ -32,8 +31,10 @@ if (search.has('username')) {
 					'<p>' + date + '</p>'
 			}
 		})
-	Promise.all([gtDate, getName, requestfetch])
+	Promise.all([gtDate, getName, requestfetch])}
+if (search.has('username')) {
+	UserInfo(userName);
 }
 else {
-	document.body.innerHTML = "ошибка: не добавлен адрес пользователя";
+	UserInfo(userNameMain);
 };
